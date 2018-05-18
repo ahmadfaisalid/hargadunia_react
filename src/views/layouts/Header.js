171820a06291e452c6d288ce525d2b7e2eb172dd
Fauzi Ferdiansyah <fetruzie2@gmail.com>
2018-05-18 14:03:52 +0700

    update mckup hargadunia react (navbar header)

--- src/views/layouts/Header.js
+++ src/views/layouts/Header.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 class Header extends Component {
   render() {
     return (
-    <section>
+    <section id="hargadunia-nav-inner">
+      <div className="overlay active"></div>
       <nav className=" navbar navbar-expand-md fixed-top navbar-light header">
       <div className="container">
         <a className="navbar-brand" href="#"><img src="https://d3ol8ih1xbmzso.cloudfront.net/asset/05-2018/banner/img-logo-5af3b8b39c9d7" className="logo"/></a>
@@ -11,12 +12,61 @@ class Header extends Component {
         </button>
         <div className="navbar-collapse offcanvas-collapse">
           <ul className="navbar-nav mr-auto">
-            <li className="nav-item">
-              <a className="nav-link" href="">Kategori</a>
+            <li className="nav-item nav-departement mr-lg-2 ml-lg-5 mr-md-2 ml-md-5">
+              <button className="nav-link btn nav-categories" id="cat-menu" type="button">Kategori</button>
+              <ul className="dropdown-menu multi-level" role="menu" aria-labelledby="dropdownMenu">
+                <li className="dropdown-submenu">
+                  <a  className="dropdown-item" tabIndex="-1" href="#">Electronics & Computers</a>
+                    <ul className="dropdown-menu menu-secondary">
+                      <li className="dropdown-item"><a href="#">Second level Second level</a></li>
+                      <li className="dropdown-item"><a href="#">Second level</a></li>
+                      <li className="dropdown-item"><a href="#">Second level</a></li>
+                    </ul>
+                </li>
+                <li className="dropdown-submenu">
+                  <a  className="dropdown-item" tabIndex="-1" href="#">Boxs</a>
+                    <ul className="dropdown-menu menu-secondary">
+                      <li className="dropdown-item"><a href="#">Second level</a></li>
+                      <li className="dropdown-item"><a href="#">Second level</a></li>
+                      <li className="dropdown-item"><a href="#">Second level</a></li>
+                    </ul>
+                </li>
+                <li className="dropdown-submenu">
+                  <a  className="dropdown-item" tabIndex="-1" href="#">Home, Garden & Tools</a>
+                    <ul className="dropdown-menu menu-secondary">
+                      <li className="dropdown-item"><a href="#">Second level</a></li>
+                      <li className="dropdown-item"><a href="#">Second level</a></li>
+                      <li className="dropdown-item"><a href="#">Second level</a></li>
+                    </ul>
+                </li>
+              </ul>
             </li>
-            <input className="form-control mr-sm-2 search-input" type="text" placeholder="Cari produk" aria-label="Search"/>
+            <div className="input-group">
+              <div className="input-group-prepend">
+                <button className="btn btn-hg dropdown-toggle btn-cat-search" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">All</button>
+                <div className="dropdown-menu">
+                  <li className="dropdown-item" >Action</li>
+                  <a className="dropdown-item" href="#">Another action</a>
+                  <a className="dropdown-item" href="#">Something else here</a>
+                  <a className="dropdown-item" href="#">Action</a>
+                  <a className="dropdown-item" href="#">Another action</a>
+                  <a className="dropdown-item" href="#">Something else here</a>
+                  <a className="dropdown-item" href="#">Action</a>
+                  <a className="dropdown-item" href="#">Another action</a>
+                  <a className="dropdown-item" href="#">Something else here</a>
+                  <a className="dropdown-item" href="#">Action</a>
+                  <a className="dropdown-item" href="#">Another action</a>
+                  <a className="dropdown-item" href="#">Something else here</a>
+                </div>
+              </div>
+              <input className="form-control search-input" type="text" placeholder="Cari produk" aria-label="Search"/>
+              <div className="input-group-append">
+                <button className="btn btn-hg" type="button">Go!</button>
+              </div>
+            </div>
+            
           </ul>
-          <form className="form-inline my-2 my-lg-0">
+          <div className="form-inline my-2 my-lg-0">
             <ul className="navbar-nav mr-auto">
               <li className="nav-item">
                 <a className="nav-link" href="">Daftar</a>
@@ -31,23 +81,23 @@ class Header extends Component {
                 <a className="nav-link" href=""><img className="icon-header" height="18" src="https://d3ol8ih1xbmzso.cloudfront.net/asset/05-2018/banner/img-cart-5af3b5e9d06cd"/></a>
               </li>
             </ul>
-          </form>
+          </div>
         </div>
       </div>
       </nav>
       <div className="nav-scroller chin shadow-sm">
         <nav className="container nav nav-underline">
           <div className="d-flex nav-block-left">
-            <a className="nav-link" href="#">All Promo</a>
-            <a className="nav-link" href="#">Sale</a>
-            <a className="nav-link" href="#">Free International Shipping</a>
-            <a className="nav-link" href="#">Ready Stock</a>
-            <a className="nav-link" href="#">New Products</a>
-            <a className="nav-link" href="#">Under 100K</a>
+            <a className="nav-link nav-under" href="#">All Promo</a>
+            <a className="nav-link nav-under" href="#">Sale</a>
+            <a className="nav-link nav-under" href="#">Free International Shipping</a>
+            <a className="nav-link nav-under" href="#">Ready Stock</a>
+            <a className="nav-link nav-under" href="#">New Products</a>
+            <a className="nav-link nav-under" href="#">Under 100K</a>
           </div>
           <div className="d-flex nav-block-right">
-            <a className="nav-link" href="#">Orange Membership</a>
-            <a className="nav-link" href="#">Jadi Bos</a>
+            <a className="nav-link nav-under" href="#">Orange Membership</a>
+            <a className="nav-link nav-under" href="#">Jadi Bos</a>
           </div>
         </nav>
       </div>
