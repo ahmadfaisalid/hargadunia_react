d16702b65c51617242e04c5d856d902794b0edb6
Fauzi Ferdiansyah <fetruzie2@gmail.com>
2018-07-06 14:57:50 +0700

    update content js

--- src/views/layouts/Contents.js
+++ src/views/layouts/Contents.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom';
+// ---------------------------------------------------- Homepage
 import Home from '../pages/home/Home';
+// ---------------------------------------------------- Promo
+import Promo from '../pages/promo/Promo';
+import DetailPromo from '../pages/promo/DetailPromo';
+
 class Contens extends Component {
   render() {
     return (
       <div className="content-area" react-section="contents">
         <Switch>
-          <Route path='/' component={Home}/>
+          <Route history={this.props.history} exact path='/' component={Home}/>
+          <Route path='/promo' component={Promo}/>
         </Switch> 
       </div>
     );
