98cf7f7c44a55e2c550116c2846282db2f7c7b32
rizkymaulana622 <rizkymaulana622@gmail.com>
2018-07-06 14:04:50 +0700

    Update dari kinoy (page blm selesai)

--- src/views/layouts/Contents.js
+++ src/views/layouts/Contents.js
@@ -1,14 +1,19 @@
 import React, { Component } from 'react';
+import { Switch, Route } from 'react-router-dom';
 import SliderHome from '../layouts/SliderHome';
 import ProductHomepage from '../layouts/ProductHomepage';
 import Category from '../pages/home/Category';
+import ReviewOrder from '../pages/orders/ReviewOrder';
 class Contens extends Component {
   render() {
     return (
       <div>
-        <SliderHome key="slider-home" />
+        <Switch>
+          <Route path='/review-order' component={ReviewOrder}/>
+        </Switch>
+        {/* <SliderHome key="slider-home" />
         <Category/>
-        <ProductHomepage key="product-homepage" className="mt-4"/>
+        <ProductHomepage key="product-homepage" className="mt-4"/> */}
         <h3 className="page-header"><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/></h3>
         <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
         <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
